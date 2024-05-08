4a19e3957601ccc1c5e3f05905f56be2f30f4749
Swattik <70145860+swat08mx@users.noreply.github.com>
2024-05-08 10:43:44 +0530

    Update linear.py

--- linear.py
+++ linear.py
@@ -1,65 +1,32 @@
-
-
 import torch
 from torch.utils.data import DataLoader
 from torch.utils.data import TensorDataset
 import torch.optim as optim
-from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from tqdm import tqdm
 from torch import nn
 import pandas as pd
-from sklearn.model_selection import GridSearchCV, KFold
 import pandas as pd
-import numpy as np
+import sklearn.metrics as metrics
+from sklearn.metrics import confusion_matrix, roc_curve, f1_score
+import seaborn as sns
 import matplotlib.pyplot as plt
-from sklearn.linear_model import LinearRegression, Ridge, Lasso
-from sklearn.model_selection import train_test_split, cross_val_score
-import statistics
+from sklearn.model_selection import train_test_split
 
-data1 = pd.read_csv("final_data.csv")
-temp = []
-for i in range(len(data1['A/C'])):
-    if data1['A/C'][i] == 'A':
-        temp.append(1)
-    else:
-        temp.append(0)
-data1.drop(['A/C', 'Sample_ID'], axis=1, inplace=True)
-temp = pd.DataFrame(temp, columns=['labels'])
-X_train, X_test, y_train, y_test = train_test_split(data1, temp, test_size=0.3, shuffle=True)
+
+data1 = pd.read_csv("gxp_dataset.csv")
 df_new = pd.read_csv("lasso_dataset.csv")
+temp = pd.DataFrame(data1['label'].to_list(), columns=['labels'])
 
-
-
-
-# lasso = Lasso()
-
-# names = data1.columns
-# print("Column Names: {}".format(names.values))
-
-
-# lasso1 = Lasso(alpha=0.001)
-# lasso1.fit(X_train, y_train)
-# lasso1_coef = np.abs(lasso1.coef_)
-# lists = lasso1_coef.tolist()
-# print(min(lists))
-# print(statistics.median(lists))
-# median = statistics.median(lists)
-# print(len(lasso1_coef))
-
-# feature_subset=np.array(names)[lasso1_coef>median]
-# print("Selected Feature Columns: {}".format(feature_subset))
-
-# df_new = data1[feature_subset]
 if torch.cuda.is_available():
     device = "cuda:0"
 else:
     device = "cpu"
-print(df_new)
+
 vals=[]
-for i in range(10):
+for i in range(1):
   temp = pd.DataFrame(temp, columns=['labels'])
-  X_train, X_test, y_train, y_test = train_test_split(df_new, temp, test_size=0.3, shuffle=True)
+  X_train, X_test, y_train, y_test = train_test_split(df_new, temp, random_state=20, test_size=0.3)
   sc = StandardScaler()
   X_train = sc.fit_transform(X_train)
   X_test = sc.transform(X_test)
@@ -72,7 +39,7 @@ for i in range(10):
   test_set = TensorDataset(X_test_tensor, y_test_tensor)
 
   train_loader = DataLoader(train_set, batch_size=32)
-  test_loader = DataLoader(test_set, batch_size=1)
+  test_loader = DataLoader(test_set, batch_size=1, shuffle=True)
 
   class Network(nn.Module):
       def __init__(self):
@@ -95,32 +62,36 @@ for i in range(10):
   model = Network()
   model.to(device)
 
-  loss_val=[]
-  running_loss=[]
-  epochs = 200
-  optimizer = optim.Adam(params=model.parameters(), lr=0.0001)
-  loss_fn = nn.CrossEntropyLoss()
-  model.train()
-  for i in range(epochs):
-      for data in tqdm(train_loader):
-          batch = tuple(t.to(device) for t in data)
-          values, labels = batch
-          output = model(values.float())
-          print(f"Output:{torch.argmax(output, dim=1)}")
-          #print(f"targets: {labels}")
-          labels = torch.squeeze(labels)
-          loss = loss_fn(output, labels)
-          loss.backward()
-          optimizer.step()
-          optimizer.zero_grad()
-          running_loss.append(loss.item())
-          #print(f"Loss is :{loss.item()}")
-      print(f"Epoch {i}")
-      total=0
-      for i in range(len(running_loss)): total+=running_loss[i]
-      loss_val.append(total/len(running_loss))
-      print(f"Loss: {total/len(running_loss)}")
+  # loss_val=[]
+  # running_loss=[]
+  # epochs = 200
+  # optimizer = optim.Adam(params=model.parameters(), lr=0.0001)
+  # loss_fn = nn.CrossEntropyLoss()
+  # model.train()
+  # for i in range(epochs):
+  #     for data in tqdm(train_loader):
+  #         batch = tuple(t.to(device) for t in data)
+  #         values, labels = batch
+  #         output = model(values.float())
+  #         print(f"Output:{torch.argmax(output, dim=1)}")
+  #         #print(f"targets: {labels}")
+  #         labels = torch.squeeze(labels)
+  #         loss = loss_fn(output, labels)
+  #         loss.backward()
+  #         optimizer.step()
+  #         optimizer.zero_grad()
+  #         running_loss.append(loss.item())
+  #         #print(f"Loss is :{loss.item()}")
+  #     print(f"Epoch {i}")
+  #     total=0
+  #     for i in range(len(running_loss)): total+=running_loss[i]
+  #     loss_val.append(total/len(running_loss))
+  #     print(f"Loss: {total/len(running_loss)}")
+  #
+  # PATH="model.pth"
+  # torch.save(model.state_dict(), PATH)
 
+  model.load_state_dict(torch.load('model.pth'))
   model.eval()
   correct = 0
   total = 0
@@ -142,8 +113,6 @@ for i in range(10):
   vals.append(accuracy)
 print(vals)
 
-
-
 var = []
 for i in range(len(pred)):
   var.append(pred[i].tolist())
@@ -158,6 +127,14 @@ label_new=[]
 for j in range(len(var_new)):
   for k in range(len(var[j])):
     label_new.append(var_new[j][0])
-from sklearn.metrics import confusion_matrix
-print(confusion_matrix(label_new, pred_new))
 
+cm = confusion_matrix(label_new, pred_new)
+sns.heatmap(cm, annot=True, fmt='g', xticklabels=['Autism', 'Control'], yticklabels=['Autism', 'Control'])
+plt.ylabel('Prediction', fontsize=13)
+plt.xlabel('Actual', fontsize=13)
+plt.title("Confusion Matrix for Feedforward network", fontsize=17)
+plt.show()
+fpr, tpr, _ = roc_curve(label_new, pred_new)
+curve = metrics.auc(fpr, tpr)
+print(f"AUC score is: {curve}")
+print(f"F1 score is: {f1_score(label_new, pred_new)}")
